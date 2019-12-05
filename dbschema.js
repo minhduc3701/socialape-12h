631f84a3e2d3dcba3f2e3ec55606cc536ee8ba33
minhduc3701 <minh.duc.3701@gmail.com>
2019-12-05 14:33:11 +0700

    organizing

---
+++ dbschema.js
@@ -0,0 +1,32 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createAt: "2019-12-05T02:24:19.594Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="/__/firebase/7.5.0/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="/__/firebase/7.5.0/firebase-analytics.js"></script>
+
+// <!-- Initialize Firebase -->
+// <script src="/__/firebase/init.js"></script>
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyD3DOlfr4xt_GZx-tBeVrLJ4-mIFe8rkiI",
+//     authDomain: "socialape-12h.firebaseapp.com",
+//     databaseURL: "https://socialape-12h.firebaseio.com",
+//     projectId: "socialape-12h",
+//     storageBucket: "socialape-12h.appspot.com",
+//     messagingSenderId: "575302739540",
+//     appId: "1:575302739540:web:19cf1bdb7a3bcf1fa52fc1",
+//     measurementId: "G-1LJ4H5RB8T"
+//   };
